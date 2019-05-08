2b4884c0185448ffd0cb76abd53a2f1ca2a7e8be
Adrian Stoica <adrianstoica@gmail.com>
2019-05-08 17:46:53 +0300

    edited

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-encoder_r_count = 0
+vol_r_count = 0
+vol_l_count = 0
 encoder_r = pyky040.Encoder(CLK=17, DT=18, SW=26)
-encoder_l_count = 0
 encoder_l = pyky040.Encoder(CLK=16, DT=20, SW=21)
 lcd = RPi_I2C_driver.lcd()
 lcd.lcd_display_string(" Loading", 2)
@@ -28,32 +28,24 @@ lcd.lcd_display_string(" Please wait...", 3)
 time.sleep(3)
 lcd.lcd_clear()
 
-def vol_up_callback(rotvalue):
+def vol_callback(rotvalue):
+    global vol_r_count
+    global vol_l_count
     if rotvalue == 1:
-        os.system("amixer -M set 'PCM' 4%+")
+        if vol_r_count < 1:
+            vol_r_count += 1
+        elif vol_r_count == 1:
+            os.system("amixer -M set 'PCM' 4%+")
+            vol_r_count = 0
+            vol_l_count = 0
     elif rotvalue == 0:
-        os.system("amixer -M set 'PCM' 4%-")
-#    global encoder_l_count
-#    global encoder_r_count
-#    if encoder_r_count < 1:
-#        encoder_r_count += 1
-#    elif encoder_r_count == 1:
-#        os.system("amixer -M set 'PCM' 4%+")
-#        encoder_r_count = 0
-#        encoder_l_count = 0
-
-def vol_down_callback(rotvalue):
-    if rotvalue == 0:
-        os.system("amixer -M set 'PCM' 4%-")
-#    global encoder_l_count
-#    global encoder_r_count
-#    if encoder_l_count < 1:
-#        encoder_l_count += 1
-#    elif encoder_l_count == 1:
-#        os.system("amixer -M set 'PCM' 4%-")
-#        encoder_l_count = 0
-#        encoder_r_count = 0
-
+        if vol_l_count < 1:
+            vol_l_count += 1
+        elif vol_l_count == 1:
+            os.system("amixer -M set 'PCM' 4%-")
+            vol_r_count = 0
+            vol_l_count = 0
+            
 def vol_toggle_callback():
     os.system("amixer -M set 'PCM' toggle")
 
@@ -70,11 +62,11 @@ def iradio_ctrl(ictrl_file=""):
    iradio_p = subprocess.Popen(["omxplayer --adev alsa --vol -300 "+ictrl_file], 
     shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
 
-def l_test(rotvalue):
-    print(rotvalue)
+def menu(rotvalue):
+    pass
 
-encoder_r.setup(scale_min=0, scale_max=1, step=1, inc_callback=vol_up_callback, 
-            dec_callback=vol_up_callback, sw_callback=vol_toggle_callback, polling_interval=1000, sw_debounce_time=300)
+encoder_r.setup(scale_min=0, scale_max=1, step=1, inc_callback=vol_callback, 
+            dec_callback=vol_callback, sw_callback=vol_toggle_callback, polling_interval=1000, sw_debounce_time=300)
 encoder_l.setup(scale_min=0, scale_max=1, step=1, inc_callback=l_test, 
             dec_callback=l_test, sw_callback=vol_toggle_callback, polling_interval=1000, sw_debounce_time=300)
 thread_enc_r = threading.Thread(target=encoder_r.watch)
